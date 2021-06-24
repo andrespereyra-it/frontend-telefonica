ab3b436324836466568b1836e596a3832fc1e4a3
andrespereyra-it <73605405+andrespereyra-it@users.noreply.github.com>
2021-06-24 14:15:30 -0300

    Add files via upload

---
+++ js/logica.js
@@ -0,0 +1,11 @@
+function alerta1() {
+  alert("Bienvenido!");
+}
+
+function alerta2() {
+  alert("Ya va a aparecer");
+}
+
+function alerta3() {
+  alert("Ya se llenó el cupo de suscriptores");
+}
